5563bdfe39c752e94b8435ea5a9c18f0abfb482a
aaygill14 <fairus50@hotmail.com>
2025-06-10 04:29:09 +0000

    Jun 10, 2025, 4:29 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -1,17 +1,22 @@
 # Write all your codes for Day 2 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day2")
+# print("Hello from Day 2")
 
 ########################################################################
 # Task 1:
-
+# for count in range(100):
+#     print("I will not sling mud at my friends.")
 
 
 ########################################################################
 # Task 2:
-
-
-
+# name = "ADAM"
+# for letter in name:
+#     print("Give me a " + letter + "!")
+# name = "Adam"
+# print(name + " is the best!")
+# name = "ADAM"
+# print(name + "!")
 ########################################################################
 # Task 3:
 
@@ -19,17 +24,20 @@ print("hello from day2")
 
 ########################################################################
 # Task 4:
-
+# for count in range (1, 11):
+#     print (count)
 
 
 ########################################################################
 # Task 5:
-
+# for count in range (5, 33):
+#     print(count)
 
 
 ########################################################################
 # Task 6:
-
+for count in range (50,0,-1):
+    print(count)
 
 
 ########################################################################
