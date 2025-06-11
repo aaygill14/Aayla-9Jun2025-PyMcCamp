25c55a125a4287d199fdbd4ab570aa63344e204f
aaygill14 <fairus50@hotmail.com>
2025-06-11 04:37:11 +0000

    Jun 11, 2025, 4:37 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -1,27 +1,40 @@
 # Write all your codes for Day 3 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day3")
+# print("Hello from Day 3")
 
 ########################################################################
 # Task 1:
+# yourname = input("What is your name?")
+# print("Hello "  + yourname)
+# yourage = input("How old are you?")
+# yourage = int(yourage) + 3
+# print("I am " + str(yourage) + " this year.")
 
-
-
+# yourname = input("What is your name?")
+# yourtitle = input("What is your title?")
+# yourcommand = input ("What is your command?")
+# print(yourtitle  + yourname + yourcommand )
 ########################################################################
 # Task 2:
-
-
-
+# print("Welcome")
+# yourname = input("What is your name?")
+# yourpurchase = input("What do you want to buy?")
+# num_pens = input("How many pens do you want to buy?")
+# print(yourname + " bought" + num_pens + " pens.")
 ########################################################################
 # Task 3:
-
-
-
+# num1 = input("Give me a 1st number: ")
+# num2 = input("Give me a 2nd number: ")
+# answer = int(num1) + int(num2)
+# print("The answer is " + str(answer))
 ########################################################################
 # Task 4:
-
-
-
+password = "passme"
+guess = input("What is the password?")
+if guess == password:
+    print("Correct!")
+else:
+    print("Password wrong...Access denied!")
 ########################################################################
 # Task 5:
 
